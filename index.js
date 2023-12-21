5f5096617581f94c8c597e359fe6e7917969ec4e
+ <+>
2023-12-20 22:38:22 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+    function saturdayFun(activity) {
+    if (activity) {
+      return "This Saturday, I want to " + activity + "!";
+    } else {
+      return "This Saturday, I want to roller-skate!";
+    }
+  }
+    
+    const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+    console.log(mondayWork());
+
+
+    function wrapAdjective(flair = "*") {
+        return function(adjective = "special") {
+          return `You are ${flair}${adjective}${flair}!`;
+        }
+      }
+      const result = wrapAdjective("*")("wonderful");
+      console.log(result); 
+      
